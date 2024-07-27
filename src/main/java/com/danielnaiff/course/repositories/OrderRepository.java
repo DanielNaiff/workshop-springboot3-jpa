9065359ba29b2a02c739e22acefb1cc3d2a3633d
DanielNaiff <danielnaiff344@gmail.com>
2024-07-27 18:55:54 -0300

    Order, Instant, ISO 8601 part 1

---
+++ src/main/java/com/danielnaiff/course/repositories/OrderRepository.java
@@ -0,0 +1,9 @@
+package com.danielnaiff.course.repositories;
+
+import com.danielnaiff.course.entities.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, Long> {
+}
