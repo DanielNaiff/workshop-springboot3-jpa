136e793ba6a33ba084077e368642133c1e237f1a
DanielNaiff <danielnaiff344@gmail.com>
2024-07-28 11:17:16 -0300

    OrderStatus enum

--- src/main/java/com/danielnaiff/course/entities/Order.java
+++ src/main/java/com/danielnaiff/course/entities/Order.java
@@ -1,5 +1,6 @@
 package com.danielnaiff.course.entities;
 
+import com.danielnaiff.course.entities.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
@@ -20,16 +21,30 @@ public class Order implements Serializable {
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
     private Instant moment;
 
+    private Integer orderStatus;
+
     @ManyToOne
     @JoinColumn(name = "client-id")
     private User client;
 
     public Order(){}
 
-    public Order(Long id, Instant moment, User client) {
+    public Order(Long id, Instant moment,OrderStatus orderStatus, User client) {
         this.id = id;
         this.moment = moment;
+        setOrderStatus(orderStatus);
         this.client = client;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(orderStatus) ;
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        if(orderStatus != null){
+            this.orderStatus = orderStatus.getCode();
+        }
+
     }
 
     public Long getId() {
